461282fcb9b5455c24aa106de502a2ebf5c242d5
Jack Ratner <jackratner@gmail.com>
2025-04-23 18:23:23 -0400

    fix: deadlocks processing messages off socket
    Unclear what was hanging, but the world mutex would start queuing
    after a while -- some message handler seemingly would freeze up for
    whatever reason and not free up the mutex guard. Maybe a deadlock,
    maybe something else.
    
    Fixes this by wrapping the message processor in a timeout function,
    effectively cancelling any lingering mutex guards after a 100ms
    timeout.

--- src/drum_circle.rs
+++ src/drum_circle.rs
@@ -1,13 +1,14 @@
-use futures::lock::Mutex;
 use futures::stream::SplitSink;
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::net::TcpStream;
-use tokio_tungstenite::WebSocketStream;
+use tokio::sync::Mutex;
 use tokio_tungstenite::tungstenite::protocol::Message;
+use tokio_tungstenite::WebSocketStream;
 
 pub type DrummerId = String;
 pub type CircleId = String;
+#[derive(Debug)]
 pub struct Drummer {
     pub id: DrummerId,
     pub stream: SplitSink<WebSocketStream<TcpStream>, Message>,
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::time::Duration;
 use std::{collections::HashMap, env, io::Error as IoError, net::SocketAddr, sync::Arc};
 
-use futures::lock::Mutex;
 use futures::SinkExt;
 use futures::StreamExt;
+use tokio::time::timeout;
+use tokio_tungstenite::tungstenite::Message;
 
 mod drum_circle;
 use crate::drum_circle::{CircleId, DrumCircle, Drummer, DrummerId};
@@ -11,13 +13,106 @@ mod message;
 use crate::message::{deserialize, serialize, WSPayload};
 
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
 
 use uuid::Uuid;
-type WorldOfCircles = Arc<Mutex<HashMap<CircleId, DrumCircle>>>;
+type WorldOfCircles = HashMap<CircleId, DrumCircle>;
 type NextCircleId = Arc<Mutex<u32>>;
 
+async fn process_message(
+    msg: Message,
+    user_mtx: Arc<Mutex<Drummer>>,
+    next_circle_id: NextCircleId,
+    world: Arc<Mutex<WorldOfCircles>>,
+) -> Result<(), tokio_tungstenite::tungstenite::Error> {
+    if msg.is_text() || msg.is_binary() {
+        let m = deserialize(msg.to_text()?);
+
+        match m.name.as_ref() {
+            "new_circle" => {
+                // create a drum circle, send response payload back
+                let mut user = user_mtx.lock().await;
+
+                // Use a block for accessing world to prevent lingering locks
+                let response = {
+                    let mut circle_idx = next_circle_id.lock().await;
+                    let circle_id = circle_idx.to_string();
+                    let mut circle = DrumCircle::new();
+
+                    *circle_idx += 1;
+
+                    circle.insert(user.id.clone(), user_mtx.clone());
+                    let mut world = world.lock().await;
+
+                    world.insert(circle_id.clone(), circle);
+
+                    let response = WSPayload {
+                        name: "circle_created".to_string(),
+                        circle_id: circle_id.into(),
+                        ..WSPayload::default()
+                    };
+                    response
+                };
+
+                user.stream.send(serialize(response)).await?;
+            }
+            "join_circle" => {
+                // find drum circle, send membership list response back
+                let mut user = user_mtx.lock().await;
+
+                let response = {
+                    let circle_id = m.circle_id.unwrap();
+                    let mut world = world.lock().await;
+
+                    let circle = world.get_mut(&circle_id).unwrap();
+
+                    // TODO: less clunky way to get a vec of strings of user ids?
+                    let mut members: Vec<DrummerId> = Vec::new();
+                    for key in circle.keys() {
+                        members.push(key.clone());
+                    }
+
+                    circle.insert(user.id.clone(), user_mtx.clone());
+
+                    let response = WSPayload {
+                        members: members.into(),
+                        name: "circle_discovery".to_string(),
+                        circle_id: circle_id.into(),
+                        ..WSPayload::default()
+                    };
+                    response
+                };
+
+                user.stream.send(serialize(response)).await?;
+            }
+            "new_member_rtc_offer" | "new_member_rtc_answer" | "ice_candidate" => {
+                let user = user_mtx.lock().await;
+                let circle_id = m.circle_id.clone().unwrap();
+                let world = world.lock().await;
+                let circle = world.get(&circle_id).unwrap();
+
+                let peer_id = m.member_id.clone().unwrap();
+                let mut peer = circle.get(&peer_id).unwrap().lock().await;
+
+                // Forward original payload, swapping originator's id for dest id
+                let response = WSPayload {
+                    member_id: user.id.clone().into(),
+                    ..m
+                };
+
+                peer.stream.send(serialize(response)).await?;
+            }
+            _ => {
+                println!("Unexpected message name: {}", m.name);
+            }
+        }
+    }
+
+    Ok(())
+}
+
 async fn handle_connection(
-    world: WorldOfCircles,
+    world: Arc<Mutex<WorldOfCircles>>,
     next_circle_id: NextCircleId,
     raw_stream: TcpStream,
     addr: SocketAddr,
@@ -39,84 +134,39 @@ async fn handle_connection(
 
     let user_mtx = user_wrapper.clone();
     while let Some(msg) = incoming.next().await {
-        let msg = msg?;
-        if msg.is_text() || msg.is_binary() {
-            let m = deserialize(msg.to_text()?);
-
-            match m.name.as_ref() {
-                "new_circle" => {
-                    println!("Making a new circle");
-                    // create a drum circle, send response payload back
-                    let mut user = user_mtx.lock().await;
-                    let mut circle_idx = next_circle_id.lock().await;
-                    let circle_id = circle_idx.to_string();
-                    let mut circle = DrumCircle::new();
-
-                    *circle_idx += 1;
-
-                    circle.insert(user.id.clone(), user_mtx.clone());
-                    world.lock().await.insert(circle_id.clone(), circle);
-
-                    let response = WSPayload {
-                        name: "circle_created".to_string(),
-                        circle_id: circle_id.into(),
-                        ..WSPayload::default()
-                    };
-
-                    user.stream.send(serialize(response)).await?;
-                }
-                "join_circle" => {
-                    // find drum circle, send membership list response back
-                    let mut user = user_mtx.lock().await;
-                    let circle_id = m.circle_id.unwrap();
-                    println!("Joining circle {}", circle_id);
-                    let mut world = world.lock().await;
-                    let circle = world.get_mut(&circle_id).unwrap();
-
-                    // TODO: less clunky way to get a vec of strings of user ids?
-                    let mut members: Vec<DrummerId> = Vec::new();
-                    for key in circle.keys() {
-                        members.push(key.clone());
-                    }
-
-                    circle.insert(user.id.clone(), user_mtx.clone());
-
-                    let response = WSPayload {
-                        members: members.into(),
-                        name: "circle_discovery".to_string(),
-                        circle_id: circle_id.into(),
-                        ..WSPayload::default()
-                    };
-
-                    user.stream.send(serialize(response)).await?;
-                }
-                "new_member_rtc_offer" | "new_member_rtc_answer" | "ice_candidate" => {
-                    println!("Got message: {:?}", m);
-                    let user = user_mtx.lock().await;
-                    let circle_id = m.circle_id.clone().unwrap();
-                    let world = world.lock().await;
-                    let circle = world.get(&circle_id).unwrap();
-                    let peer_id = m.member_id.clone().unwrap();
-                    println!("Peer id is {}", peer_id);
-                    let mut peer = circle.get(&peer_id).unwrap().lock().await;
-
-                    // Forward original payload, swapping originator's id for dest id
-                    let response = WSPayload {
-                        member_id: user.id.clone().into(),
-                        ..m
-                    };
-                    peer.stream.send(serialize(response)).await?;
-                }
-                _ => {
-                    println!("Unexpected message name: {}", m.name);
-                }
-            }
+        if let Err(_) = timeout(
+            Duration::from_millis(100),
+            process_message(
+                msg?,
+                user_mtx.clone(),
+                next_circle_id.clone(),
+                world.clone(),
+            ),
+        )
+        .await
+        {
+            println!("Failed to process msg within 100ms")
         }
     }
 
-    println!("{} disconnected", &addr);
-    // TODO: Remove member from their circle
-    // peer_map.lock().unwrap().remove(&addr);
+    let user = user_mtx.lock().await;
+    let to_delete_id = user.id.clone();
+    println!(
+        "{} {} disconnected, removing from circle",
+        &addr, to_delete_id
+    );
+    let mut world = world.lock().await;
+    let circle = world
+        .values_mut()
+        .find(|circle| circle.keys().any(|id| *id == user.id));
+    match circle {
+        Some(c) => {
+            c.remove(&to_delete_id);
+            println!("Removed user");
+        }
+        None => println!("No circle found to remove disconnecting user from."),
+    }
+
     Ok(())
 }
 
@@ -126,7 +176,7 @@ async fn main() -> Result<(), IoError> {
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".to_string());
 
-    let world = WorldOfCircles::new(Mutex::new(HashMap::new()));
+    let world = Arc::new(Mutex::new(WorldOfCircles::new()));
     let next_circle_id = Arc::new(Mutex::new(0));
 
     // Create the event loop and TCP listener we'll accept connections on.
